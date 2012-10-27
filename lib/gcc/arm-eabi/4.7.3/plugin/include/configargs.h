bc54c7449cafe00d24aedeec1c17546d23e8c77e
synergydev <synergye@codefi.re>
2012-10-27 04:36:44 +0000

    CFX-Toolchain 4.7.3-2012.10.26 arm-eabi Source can be found here: github.com/CFX-Toolchain Features: 	[*] Simulator target and tui disabled 	[*] Graphite enabled 	[*] Linaro GCC version 4.7-2012.10 	[*] Binutils version 2.22 	[*] GMP version 5.0.5 	[*] MPFR version 2.4.2 	[*] Cloog version 0.16.3 	[*] GDB version 7.3.x 	[*] Otherwise based on AOSP toolchain master

---
+++ lib/gcc/arm-eabi/4.7.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../build/../gcc/gcc-linaro-4.7-2012.10/configure --prefix=/tmp/android-toolchain-eabi --target=arm-eabi --host=x86_64-unknown-linux-gnu --build=x86_64-unknown-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-pkgversion=cfX-4.7.2 --with-gmp=/home/synergy/storage/toolchains/cfx-toolchain/cfx/temp-install --with-mpfr=/home/synergy/storage/toolchains/cfx-toolchain/cfx/temp-install --with-mpc=/home/synergy/storage/toolchains/cfx-toolchain/cfx/temp-install --without-ppl --without-cloog --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --disable-libitm --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --prefix=/tmp/android-toolchain-eabi --enable-graphite=yes --with-gdb-version=7.3.x --with-cloog-version=0.16.3 --with-mpfr-version=2.4.2 --with-gmp-version=5.0.5 --with-binutils-version=2.22 --with-gcc-version=linaro-4.7-2012.10 --program-transform-name='s&^&arm-eabi-&'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
